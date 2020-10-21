da38345fbeb297365ea3892b8adc3f1d56cee2b4
Danieldong0802 <1756330108@qq.com>
2020-10-21 17:05:38 +0800

    [MOD]更新SERVICE-HI

--- service-hi/src/main/java/com/forezp/servicehi/ServiceHiApplication.java
+++ service-hi/src/main/java/com/forezp/servicehi/ServiceHiApplication.java
@@ -2,17 +2,12 @@ package com.forezp.servicehi;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaClient
-@RestController
 public class ServiceHiApplication {
 
 	private static final Logger logger = LoggerFactory.getLogger(ServiceHiApplication.class);
@@ -22,15 +17,4 @@ public class ServiceHiApplication {
 		SpringApplication.run(ServiceHiApplication.class, args);
 		logger.info("服务提供者SERVICE-HI started......");
 	}
-
-
-	@Value("${server.port}")
-	String port;
-
-
-	@RequestMapping("/hi")
-	public String home(@RequestParam(value="name", defaultValue = "forezp") String name) {
-		return "hi " + name + ", i am from port:" + port;
-	}
-
 }
